185131815eb5dedf0f8838c49d0a46e5c12f9bbf
Justin Ling <48165446+justinrling@users.noreply.github.com>
2020-09-03 20:05:15 -0700

    Performance Improvement
    Modified so OrderSummary component only updates when Modal is shown

--- burger-builder/src/hoc/Layout/Layout.js
+++ burger-builder/src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';     //modular css classes
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
                                         //remember: component cannot return multiple elements
                                         //must wrap in <div>, <aux> higher order component, or make array
